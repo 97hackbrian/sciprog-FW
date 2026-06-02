f4e38e6c58711b19e18c8e8d201feedfc6724f0a
Diego Urrutia-Astorga <mika.cl@gmail.com>
2026-06-02 15:54:33 -0400

    feat: add multiprocessing support for prime number counting

---
+++ scripts/run_primes_multiprocessing.py
@@ -0,0 +1,100 @@
+#  Copyright (c) 2026. Programacion Cientifica, DISC, Antofagasta, Chile.
+import logging
+import multiprocessing
+from pathlib import Path
+
+from numba.core.decorators import njit
+from tqdm import tqdm
+
+from benchmarking import benchmark  # ty:ignore[unresolved-import]
+from logger import configure_logging  # ty:ignore[unresolved-import]
+
+
+@njit(fastmath=True)
+def is_prime(num: int):
+    # less than 2 always false
+    if num < 2:
+        return False
+    # if exactly 2 -> true
+    if num == 2:
+        return True
+    # multiple of two or three -> false
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+
+    # try all the values
+    for i in range(5, int(num ** 0.5) + 1, 6):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+def count_primes_in_range(the_range: tuple[int, int]) -> int:
+    primes = 0
+    start, end = the_range
+    for i in range(start, end):
+        if is_prime(i):
+            primes += 1
+    return primes
+
+
+def main() -> None:
+    n = 10 * 1000 * 1000
+    log.debug(f"Counting primes from 1 to {n:,} ..")
+
+    # retrieve the number of cores
+    num_cores = 22  # os.cpu_count()
+    log.debug(f"num_cores: {num_cores}.")
+
+    # the number of work to do
+    num_chunks = num_cores * 20
+
+    # the size of each work
+    chunk_size = n // num_cores
+
+    # divide the space in (start, end)
+    ranges = []
+    for i in range(num_chunks):
+        start = i * chunk_size
+        if i == num_chunks - 1:
+            end = n
+        else:
+            end = (i + 1) * chunk_size
+        ranges.append((start, end))
+
+    for start, end in ranges:
+        log.debug(f"range: from {start} -> {end}")
+
+    # the pool of workers
+    with multiprocessing.Pool(num_cores) as pool:
+        result = list(tqdm(
+            pool.imap(count_primes_in_range, ranges),
+            total=len(ranges),
+            ncols=250,
+            desc="count primes"
+        ))
+    primes = sum(result)
+    log.debug(f"primes: {primes:,}")
+
+
+# call the main function
+if __name__ == '__main__':
+    # configure the logging
+    configure_logging(logging.DEBUG)
+    # get the main logger
+    log = logging.getLogger(__name__)
+
+    # get the root directory
+    root_dir = Path(__file__).resolve().parent.parent
+    log.debug(f"root_dir: {root_dir}")
+
+    # get the output directory
+    output_dir = root_dir / "output"
+    log.debug(f"output_dir: {output_dir}")
+
+    # measure time
+    with benchmark("main", log):
+        log.info("️🏎️ starting ..")
+        main()
+        log.info("️🏁 done.")
